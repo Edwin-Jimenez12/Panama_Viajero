a29d7edc728e5b0bc98d77768e468d0eea66bc17
Edwin-Jimenez12 <ed.jimenez0012@gmail.com>
2026-05-08 11:04:38 -0500

    Se inicia proceso de mapa

--- src/layout/map/DinamicMap.jsx
+++ src/layout/map/DinamicMap.jsx
@@ -1,26 +1,47 @@
 import { Link } from 'react-router-dom';
-import { MapPin } from 'lucide-react';
+import MapGenera from '../../assets/srcProvincias/Mpa.svg';
+import LosSantos from '../../assets/srcProvincias/los_santos/los santos.svg';
+import Chiriqui from '../../assets/srcProvincias/chiriqui/chiriqui.svg';
+import BocasDelToro from '../../assets/srcProvincias/bocas_del_toto/bocas del toro.svg';
+import NgabeBugle from '../../assets/srcProvincias/comarca_ngabe_bugle/comarca_ngabe_bugle.svg';
+import Cocle from '../../assets/srcProvincias/cocle/cocle.svg';
+import Colon from '../../assets/srcProvincias/colon/colon.svg';
+import GunaYala from '../../assets/srcProvincias/comarca_guna_yala/guna_yala.svg';
+import Darien from '../../assets/srcProvincias/darien/darien.svg';
 
 function Map() {
     return (
         <div className="mx-auto my-28 flex max-w-6xl flex-col items-center px-4 text-center md:my-40">
-            <div className="w-full rounded-xl bg-brand-white p-8 shadow-[0_18px_45px_rgba(77,76,76,0.16)] md:p-12">
-                <div className="mx-auto flex h-14 w-14 items-center justify-center rounded-full bg-brand-blue text-brand-white">
-                    <MapPin size={28} />
+            <div className="w-full rounded-xl  md:p-12">
+                {/* map */}
+                
+                <div className='relative drop-shadow-[0_18px_45px_rgba(77,76,76,0.50)]'>
+                    <img src={MapGenera} alt="" className='opacity-50'/>
+                    {/* Provincias imagenes */}
+                    <img src={LosSantos} alt="" className='absolute top-[292px] left-[409px] w-[120px] h-auto cursor-pointer transition hover:scale-110 duration-300' />
+                    <img src={Chiriqui} alt="" className='absolute top-[131px] left-[0px] w-[263px] h-auto cursor-pointer transition hover:scale-110 duration-300' />
+                    <img src={BocasDelToro} alt="" className='absolute top-[1px] left-[13px] w-[121px] h-auto cursor-pointer transition hover:scale-110 duration-300' />
+                    <img src={NgabeBugle} alt="" className='absolute top-[85px] left-[110px] w-[212px] h-auto cursor-pointer transition hover:scale-110 duration-300' />
+                    <img src={Cocle} alt="" className='absolute top-[111px] left-[395px] w-[125px] h-auto cursor-pointer transition hover:scale-110 duration-300' />
+                    <img src={Colon} alt="" className='absolute top-[0px] left-[385px] w-[308px] h-auto cursor-pointer transition hover:scale-110 duration-300' />
+                    <img src={GunaYala} alt="" className='absolute top-[30px] left-[663px] w-[320px] h-auto cursor-pointer transition hover:scale-110 duration-300' />
+                    <img src={Darien} alt="" className='absolute top-[100px] left-[805px] w-[220px] h-auto cursor-pointer transition hover:scale-110 duration-300' />
+
                 </div>
-
-                <h1 className="mt-6 font-invisible text-3xl font-bold text-brand-blue md:text-5xl">
-                    Provincias por descubrir
-                </h1>
-
-                <p className="mx-auto mt-4 max-w-2xl leading-7 text-brand-charcoal/80 md:text-lg">
-                    Explora rutas, cultura y puntos turisticos organizados por provincia.
-                    Esta seccion mantendra el mapa como punto central de navegacion.
-                </p>
+                <Link
+                    to="/provincias/bocas-del-toro"
+                    className="mt-8 inline-flex rounded-full bg-brand-red px-6 py-3 
+                    font-semibold text-brand-white shadow-[0_12px_25px_rgba(205,46,76,0.28)] 
+                    transition hover:bg-brand-blue"
+                >
+                    Ver Bocas del Toro
+                </Link>
 
                 <Link
                     to="/provincias/bocas-del-toro"
-                    className="mt-8 inline-flex rounded-full bg-brand-red px-6 py-3 font-semibold text-brand-white shadow-[0_12px_25px_rgba(205,46,76,0.28)] transition hover:bg-brand-blue"
+                    className="mt-8 inline-flex rounded-full bg-brand-red px-6 py-3 
+                    font-semibold text-brand-white shadow-[0_12px_25px_rgba(205,46,76,0.28)] 
+                    transition hover:bg-brand-blue"
                 >
                     Ver Bocas del Toro
                 </Link>
