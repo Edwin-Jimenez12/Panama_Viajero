fd7f16448740458396ac0cb63fffbeeade666c65
Edwin-Jimenez12 <ed.jimenez0012@gmail.com>
2026-05-15 16:10:40 -0500

    Mapa listo, falta problema de hover

--- src/layout/map/DinamicMap.jsx
+++ src/layout/map/DinamicMap.jsx
@@ -13,17 +13,17 @@ import Panama from '../../assets/components/prov_pages/PanamaSvg.jsx';
 import PanamaOeste from '../../assets/components/prov_pages/PanamaOesteSvg.jsx';
 
 const provinces = [
-    { component: LosSantos, alt: 'Los Santos', to: '/provincias/los-santos', top: '68.87%', left: '39.90%', width: '11.71%' },
-    { component: ChiriquiSvg, alt: 'Chiriqui', to: '/provincias/chiriqui', top: '30.90%', left: '0%', width: '25.66%' },
-    { component: BocasDelToroSvg, alt: 'Bocas del Toro', to: '/provincias/bocas-del-toro#video', top: '0.24%', left: '1.27%', width: '11.80%' },
+    { component: LosSantos, alt: 'Los Santos', to: '/provincias/los-santos', top: '68.87%', left: '39.90%', width: '11.71%', zIndex: 24 },
+    { component: ChiriquiSvg, alt: 'Chiriqui', to: '/provincias/chiriqui', top: '28.5%', left: '-0.20%', width: '25.66%' },
+    { component: BocasDelToroSvg, alt: 'Bocas del Toro', to: '/provincias/bocas-del-toro#video', top: '-3%', left: '0.50%', width: '32%' },
     { component: Cocle, alt: 'Cocle', to: '/provincias/cocle', top: '12%', left: '34.75%', width: '21%' },
-    { component: Colon, alt: 'Colon', to: '/provincias/colon', top: '0%', left: '37.56%', width: '30.05%' },
-    { component: GunaYala, alt: 'Guna Yala', to: '/provincias/comarca-guna-yala', top: '7.08%', left: '64.68%', width: '31.22%' },
-    { component: Darien, alt: 'Darien', to: '/provincias/darien', top: '23.58%', left: '78.54%', width: '21.46%' },
-    { component: Veraguas, alt: 'Veraguas', to: '/provincias/veraguas', top: '26%', left: '20.85%', width: '23.60%' },
-    { component: Herrera, alt: 'Herrera', to: '/provincias/herrera', top: '58.50%', left: '29%', width: '21%' },
-    { component: PanamaOeste, alt: 'Panama Oeste', top: '12.2%', left: '46.35%', width: '15.5%' },
-    { component: Panama, alt: 'Panama', to: '/provincias/panama', top: '0%', left: '56.5%', width: '30.80%' },
+    { component: Colon, alt: 'Colon', to: '/provincias/colon', top: '0%', left: '37.56%', width: '30.05%', zIndex: 26 },
+    { component: GunaYala, alt: 'Guna Yala', to: '/provincias/comarca-guna-yala', top: '7.08%', left: '64.68%', width: '31.22%', zIndex: 27 },
+    { component: Darien, alt: 'Darien', to: '/provincias/darien', top: '18.5%', left: '77.2%', width: '23.5%' },
+    { component: Veraguas, alt: 'Veraguas', to: '/provincias/veraguas', top: '26%', left: '20.85%', width: '23.60%', zIndex: 22 },
+    { component: Herrera, alt: 'Herrera', to: '/provincias/herrera', top: '58.50%', left: '28.85%', width: '20.85%', zIndex: 23 },
+    { component: PanamaOeste, alt: 'Panama Oeste', top: '12.2%', left: '46.35%', width: '15.5%', zIndex: 28 },
+    { component: Panama, alt: 'Panama', to: '/provincias/panama', top: '0%', left: '56.5%', width: '30.80%', zIndex: 29 },
 ];
 
 function Map() {
@@ -36,7 +36,7 @@ function Map() {
                 {/* map */}
                 <div className='flex flex-col'>
                     <label className='font-main text-5xl text-brand-blue'>Panamá</label>
-                    <label className='font-secondary-italic' >Un Pais de union</label>
+                    <label className='font-secondary' >Un Pais de union</label>
                 </div>
                 <div className='relative  aspect-[1025/424] drop-shadow-[0_18px_45px_rgba(77,76,76,0.50)] mb-[-25px]'>
                     {/* Provincias imagenes */}
@@ -49,7 +49,8 @@ function Map() {
                                     top: province.top,
                                     left: province.left,
                                     width: province.width,
-                                    transform: activeProvince === province.alt ? 'scale(1.1)' : 'scale(1)',
+                                    zIndex: activeProvince === province.alt ? 50 : (province.zIndex ?? 1),
+                                    transform: activeProvince === province.alt ? 'scale(1.1) ' : 'scale(1)',
                                 }}
                             >
                                 {province.component ? (
@@ -69,6 +70,7 @@ function Map() {
                                     top: province.top,
                                     left: province.left,
                                     width: province.width,
+                                    zIndex: province.zIndex ?? 1,
                                 }}
                             >
                                 {province.component ? <province.component className="h-auto w-full" /> : <img src={province.src} alt={province.alt} className="h-auto w-full" />}
@@ -77,7 +79,7 @@ function Map() {
                     ))}
                 </div>
             </div>
-            <label className='font-secondary-italic text-brand-red mt-10 md:mt-0 text-xs md:text-lg'>Elige la provincia a visitar</label>
+            <label className='font-secondary text-brand-red mt-10 md:mt-0 text-xs md:text-lg'>Elige la provincia a visitar</label>
         </div>
     )
 }
