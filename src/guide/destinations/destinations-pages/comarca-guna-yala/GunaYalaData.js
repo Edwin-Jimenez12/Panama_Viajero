18129ac44020523966912d995f3b24b490939ba3
Edwin-Jimenez12 <ed.jimenez0012@gmail.com>
2026-07-13 16:57:09 -0500

    Purga de multimedia no usada

--- src/guide/destinations/destinations-pages/comarca-guna-yala/GunaYalaData.js
+++ src/guide/destinations/destinations-pages/comarca-guna-yala/GunaYalaData.js
@@ -5,6 +5,8 @@ export const provincias = [
     banner: {
       tipo: 'video',
       src: '/videos/guna_yala/GunaYalaBanner.av1.mp4',
+      webmSrc: '/videos/guna_yala/GunaYalaBanner.webm',
+      fallbackSrc: '/videos/guna_yala/GunaYalaBanner.h264.mp4',
       alt: 'Vista de la Comarca Guna Yala',
     },
     descripcionCorta:
@@ -13,14 +15,15 @@ export const provincias = [
       src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1825591.6817842708!2d-79.64365842487119!3d9.052276123298334!2m3!1f0!2f0!3f0!2m3!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e513dd4281b06cf%3A0x13d9b0240ba4f630!2sGuna%20Yala%20Comarca!5e1!3m2!1sen!2spa!4v1779311689200!5m2!1sen!2spa',
     },
     actividades: [
-      'Playa',
       'Snorkel',
+      'Buceo',
+      'Kayak',
+      'Paddleboard',
       'Paseo en bote',
-      'Excursión',
-      'Baño',
-      'Estrellas de mar',
       'Fotografía',
+      'Cultura guna',
       'Gastronomía',
+      'Estrellas de mar',
     ],
     displayMode: 'sites-only',
     directSiteIds: ['isla-chichime', 'isla-perro', 'la-piscina-natural'],
